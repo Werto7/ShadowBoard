6e1d22b990516ef81f96168d788d43cd1b62fd39
Werto7 <nemoschmidt8@gmail.com>
2024-10-22 19:40:15 +0200

    Add files via upload

--- lang/German/common.php
+++ lang/German/common.php
@@ -1,1 +1,7 @@
-
+<?php
+$lang_common = array(
+//Fehlermeldung
+'Forum error header'		=> 'Entschuldigung! Die Seite konnte nicht geladen werden.',
+'Forum error description'	=> 'Dies ist wahrscheinlich ein vorübergehender Fehler. Aktualisieren Sie einfach die Seite und versuchen Sie es erneut. Wenn das Problem weiterhin besteht, versuchen Sie es bitte in 5–10 Minuten erneut.',
+);
+?>
