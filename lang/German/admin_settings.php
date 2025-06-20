bced8d1ce5af8d6773920f6e0c75e37c066c2ded
Werto7 <nemoschmidt8@gmail.com>
2025-06-20 09:18:47 +0200

    Add files via upload

---
+++ lang/German/admin_settings.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang_admin_settings = array(
+//Wartungsabschnitt
+'Maintenance message default'	=>	'Die Foren sind wegen Wartungsarbeiten vorübergehend nicht erreichbar. Bitte versuche es in ein paar Minuten erneut.<br /><br />Administrator'
+);
---
+++ lang/German/install.php
@@ -0,0 +1,14 @@
+<?php
+
+// Language definitions used in install.php
+$lang_install = array(
+//Wird bei der Installation verwendet
+'Default announce heading'	=>	'Beispielankündigung',
+'Default announce message'	=>	'<p>Gebe hier deine Ankündigung ein.</p>',
+'Default rules'				=>	'Gebe hier deine Regeln ein.',
+'Default category name'		=>	'Testkategorie',
+'Default forum name'		=>	'Testforum',
+'Default forum descrip'		=>	'Dies ist nur ein Testforum',
+'Default topic subject'		=>	'Testpost',
+'Default post contents'		=>	'Wenn du dir das hier ansiehst (was ich vermute), scheint die Installation von ShadowBoard funktioniert zu haben! Melde dich jetzt an und gehe zum Administrationsbereich, um dein Forum zu konfigurieren.'
+);
